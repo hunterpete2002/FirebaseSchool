8073eebf1881dcb7458c955a3024727ae1b810db
Petri Kilpelainen <hunterpete2002@gmail.com>
2024-03-13 05:39:20 +0200

    Mission done

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { MESSAGES, addDoc, collection, firestore, serverTimestamp, QuerySnapshot, onSnapshot, query, orderBy } from './firebase/Config';
+import { useEffect, useState } from 'react';
+import { convertFirebaseTimeStampToJS } from './helper/Functions';
 
 export default function App() {
+  const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+
+
+  const save = async() => {
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: newMessage,
+      created: serverTimestamp()
+    }).catch (error => console.log(error))
+    setNewMessage('')
+    console.log('Message saved.')
+  }
+
+  useEffect(() => {
+    const q = query(collection(firestore,MESSAGES),orderBy('created', 'desc'))
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const tempMessages = []
+
+      querySnapshot.forEach((doc) => {
+        const messageObject = {
+          id: doc.id,
+          text: doc.data().text,
+          created: convertFirebaseTimeStampToJS(doc.data().created)
+        }
+        tempMessages.push(messageObject)
+      })
+      setMessages(tempMessages)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {
+          messages.map((message) =>(
+            <View style={styles.message} key={message.id}>
+              <Text style={styles.messageInfo}>{message.created}</Text>
+              <Text>{message.text}</Text>
+            </View>
+          ))
+        }
+        <TextInput placeholder='Send message...' value={newMessage} onChangeText={text => setNewMessage(text)} />
+        <Button title='Send' type='button' onPress={save} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  message: {
+      padding: 10,
+      marginTop: 10,
+      marginBottom: 10,
+      backgroundColor: '#f5f5f5',
+      borderColor: '#ccc',
+      borderWidth: 1,
+      borderRadius: 5,
+      marginLeft: 10,
+      marginRight: 10
+  },
+  messageInfo: {
+    fontSize: 12
+  }
+
 });
